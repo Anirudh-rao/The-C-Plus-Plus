09ec5b2790b2f96afbacdc1bb5524c537ece00ff
Anirudh Rao <65554770+Anirudh-rao@users.noreply.github.com>
2026-05-07 13:53:41 +0530

    BooleanTypes in C

--- C/1.Basics/BooleanTypes.c
+++ C/1.Basics/BooleanTypes.c
@@ -1,2 +1,10 @@
-#include<stdio.h>
-#incl;ude
+#include <stdio.h>
+#include <stdbool.h>  // not needed in C23
+
+int main(void) {
+    bool x = true;
+
+    if (x) {
+        printf("x is true!\n");
+    }
+}
